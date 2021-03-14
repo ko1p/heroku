a227a470822df507dcb68f12ac25457b5cd95cd0
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-03-14 18:48:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+    </ul>
+    <h2>Home page</h2>
+`)
+})
+
+app.get('/about', (req, res) => {
+    res.send(`
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+    </ul>
+    <h2>About page</h2>
+`)
+})
+
+app.listen(3000, () => {
+    console.log('Server started!')
+})
